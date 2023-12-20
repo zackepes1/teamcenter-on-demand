90008a542e7c5557ae03e9068895eeb97b34683d
Epes <zachary.epes@siemens.com>
2023-12-20 12:21:12 -0500

    something:

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,7 +54,7 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-if(eventType === 'user.created') {
+  if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
